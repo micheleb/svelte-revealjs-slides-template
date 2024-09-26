8302dce57344dcba7b44c1acf2243127b4f45a1b
Michele Bonazza <michele.bonazza@proton.me>
2024-09-26 09:54:27 +0200

    Add Markdown

--- src/lib/deck/presentation.svelte
+++ src/lib/deck/presentation.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Code from './code.svelte';
+	import Markdown from './markdown.svelte';
 	import Notes from './notes.svelte';
 	import Slide from './slide.svelte';
 </script>
@@ -18,6 +19,29 @@
 	<p>Slide!</p>
 	<p>With <a href="https://github.com" target="_blank">links</a>!</p>
 </Slide>
+
+<Slide animate>
+	<p>First</p>
+	<p>Slide!</p>
+	<p>With <a href="https://github.com" target="_blank">links</a>!</p>
+	<p>Press <span class="font-mono bg-slate-200 text-indigo-900 px-2 py-1">s</span> for Speaker Notes</p>
+	<Notes>Here are some speaker notes, too</Notes>
+</Slide>
+
+<Markdown>
+	{`
+# Markdown Slide
+- This is another slide
+- and it's written in Markdown!
+
+---
+
+# Another Markdown Slide
+- You can create multiple slides at once!
+	`}
+</Markdown>
+
+<Markdown external name="external-example.md" />
 
 <Slide animate>
 	<Code id="code" lines="|2">
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -4,7 +4,10 @@ export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 
 	theme: {
-		extend: {}
+		extend: {},
+			fontFamily: {
+				mono: ['"JetBrains Mono"']
+			}
 	},
 
 	// eslint-disable-next-line @typescript-eslint/no-require-imports
