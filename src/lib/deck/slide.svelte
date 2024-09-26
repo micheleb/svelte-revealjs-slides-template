15aa04fc6e61b80387d34074d503dc353bcbac4d
Michele Bonazza <michele.bonazza@proton.me>
2024-09-26 16:48:04 +0200

    Add uncounted prop for slides

--- src/lib/deck/slide.svelte
+++ src/lib/deck/slide.svelte
@@ -1,13 +1,20 @@
 <script lang="ts">
 	export let id: string | null = null;
 	export let animate = false;
+	export let backgroundImage: string | null = null;
 	export let restart = false;
+	export let transition: "slide" | "fade" | null = null;
+	export let uncounted = false;
 </script>
 
 <section
 	data-auto-animate={animate || null}
 	data-auto-animate-id={id}
 	data-auto-animate-restart={restart || null}
+	data-background-image={backgroundImage || null}
+	data-transition={transition || null}
+	data-visibility={uncounted ? "uncounted" : null}
 >
 	<slot />
 </section>
+
