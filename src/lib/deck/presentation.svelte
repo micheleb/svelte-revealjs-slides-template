a45476106bde7e51075752048aa6f5ed60a9bebb
Michele Bonazza <michele.bonazza@proton.me>
2024-09-27 10:09:59 +0200

    Add Fragment, vertical slides example

--- src/lib/deck/presentation.svelte
+++ src/lib/deck/presentation.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
 	import Code from './code.svelte';
+	import Fragment from './fragment.svelte';
 	import Markdown from './markdown.svelte';
 	import Notes from './notes.svelte';
 	import Slide from './slide.svelte';
@@ -26,6 +27,30 @@
 	<p>With <a href="https://github.com" target="_blank">links</a>!</p>
 	<p>Press <span class="font-mono bg-slate-200 text-indigo-900 px-2 py-1">s</span> for Speaker Notes</p>
 	<Notes>Here are some speaker notes, too</Notes>
+</Slide>
+
+<Slide animate>
+	<Fragment animation="highlight-current-blue"><p>fragments</p></Fragment>
+	<Fragment animation="fade-in-then-semi-out"><p>are also</p></Fragment>
+	<Fragment animation="fade-in-then-out"><p>supported</p></Fragment>
+	<Fragment animation="fade-right"><p>with animations, too</p></Fragment>
+</Slide>
+
+<Slide>
+	<Slide animate>
+		<h3>Vertical slides</h3>
+	</Slide>
+
+	<Slide animate>
+		<h3>Vertical slides</h3>
+		<p>Nest your slides</p>
+	</Slide>
+
+	<Slide animate>
+		<h3>Vertical slides</h3>
+		<p>Nest your slides</p>
+		<p>And you can navigate vertically</p>
+	</Slide>
 </Slide>
 
 <Markdown>
--- static/external-example.md
+++ static/external-example.md
@@ -18,3 +18,13 @@
 
 - It even works
 - with animations!
+
+---
+
+<!-- .slide: data-auto-animate -->
+
+# For multiple slides
+
+- It even works
+- with animations!
+- (a bit annoying to write, but still)
